91fb83dca6b854a2e38780cc10c4d9ced0f8fdfb
dbagley23 <dale@reparteegallery.com>
2015-08-22 12:01:50 -0600

    asdf

--- js/app.js
+++ js/app.js
@@ -4,4 +4,42 @@ app.config(function($routeProvider, $httpProvider){
   $httpProvider.interceptors.push('httpRequestInterceptor');
 
   //router here
+  $routeProvider
+  .when('/', {
+    templateUrl: 'js/home/homeTmpl.html',
+    controller: 'homeCtrl'
+  })
+  .when('/teams/:team', {
+    templateUrl: 'js/teams/teamTmpl.html',
+    controller: 'teamCtrl',
+    resolve: {
+      teamData: function(teamService, $route){
+        debugger;
+        return teamService.getTeamData($route.current.params.team)
+      }
+    }
+  })
+  .otherwise({
+    redirectTo: '/'
+  })
+  
 });
+
+// app.config(function($routeProvider){
+//   $routeProvider
+//     .when('/', {
+//       templateUrl: 'js/home/homeTmpl.html',
+//       controller: 'homeCtrl'
+//     })
+//     .when('/settings', {
+//       templateUrl: 'js/settings/settingsTmpl.html',
+//       controller: 'settingsCtrl'
+//     })
+//     .when('/products/:id', {
+//       templateUrl: 'js/products/productTmpl.html',
+//       controller: 'productsCtrl'
+//     })
+//     .otherwise({
+//       redirectTo: '/'
+//     })
+// });
--- js/teams/teamCtrl.js
+++ js/teams/teamCtrl.js
@@ -1,5 +1,44 @@
 var app = angular.module('nbaRoutes');
 
-app.controller('teamCtrl', function($scope, $routeParams, teamService){
-
+app.controller('teamCtrl', function($scope, $routeParams, teamService, teamData){
+  if ($routeParams.team === 'utahjazz') {
+    $scope.homeTeam = 'Utah Jazz';
+    $scope.logoPath = 'images/jazz-logo.png';
+  } else if ($routeParams.team === 'losangeleslakers') {
+    $scope.homeTeam = 'Los Angeles Lakers';
+    $scope.logoPath = 'images/lakers-logo.png';
+  } else if ($routeParams.team === 'miamiheat') {
+    $scope.homeTeam = 'Miami Heat';
+    $scope.logoPath = 'images/heat-logo.png';
+  }
+  $scope.teamData = teamData;
+	  var currentTime = new Date();
+  $scope.newGame = {homeTeam: $scope.homeTeam.split(' ').join('').toLowerCase(),
+    homeTeamScore: $scope.homeTeamScore,
+    opponentScore: $scope.opponentTeamScore,
+    };
+  $scope.showNewGameForm = false;
+  $scope.toggleNewGameForm = function(){
+    if ($scope.showNewGameForm === false){
+      $scope.showNewGameForm = true;
+    } else {
+      $scope.showNewGameForm = false;
+    }
+  }
+  
+  $scope.submitGame = function(){
+    $scope.newGame.homeTeamScore = $scope.homeTeamScore;
+    $scope.newGame.opponentScore = $scope.opponentTeamScore;
+    $scope.newGame.opponent = $scope.visitors;
+    debugger;
+    teamService.addNewGame($scope.newGame).then(function(data){
+      debugger;
+      teamService.getTeamData($scope.newGame.homeTeam)
+    })
+    
+  }
+ $scope.visitors = '';  
+ $scope.homeScore = '';
+ $scope.opponentScore = ''; 
+  
 });
